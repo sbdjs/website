efc5abfc53256344432e1155c21468aeee15e222
baptiste <baptiste.lizeul@epsi.fr>
2021-10-06 11:57:33 +0200

    init project

---
+++ sbdjs/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+  const {
+    loginWithPopup, 
+    loginWithRedirect, 
+    logout, 
+    user, 
+    isAuthenticated
+  } = useAuth0();
+  
+  return (
+    <div className="App">
+      <h1>Autho0 auth</h1>
+      <ul>
+        <li>
+          <button onClick={loginWithPopup}>Login with Popup</button>
+        </li>
+        <li>
+          <button onClick={loginWithRedirect}>Login with Redirect</button>
+        </li>
+        <li>
+          <button onClick={logout}>Logout</button>
+        </li>
+      </ul>
+      <h3>User is { isAuthenticated ? "Logged" : "Not logged"}</h3>
+      <pre style={{textAlign: 'start'}}>
+        {JSON.stringify(user, null, 2)}
+      </pre>
+    </div>
+  );
+}
+
+export default App;
