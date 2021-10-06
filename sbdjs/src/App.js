9cd98d1fec61d61b56f8b8b490dcea0b1aa676e1
baptiste <baptiste.lizeul@epsi.fr>
2021-10-06 17:31:48 +0200

    tailwindcss + view & display btn if auth = true

--- sbdjs/src/App.js
+++ sbdjs/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
 
@@ -16,13 +16,13 @@ function App() {
       <h1>Autho0 auth</h1>
       <ul>
         <li>
-          <button onClick={loginWithPopup}>Login with Popup</button>
+          <button className={ isAuthenticated ? "hidden" : "bg-blue-500" } onClick={loginWithPopup}>Login with Popup</button>
         </li>
         <li>
-          <button onClick={loginWithRedirect}>Login with Redirect</button>
+          <button className={ isAuthenticated ? "hidden" : "bg-blue-500" } onClick={loginWithRedirect}>Login with Redirect</button>
         </li>
         <li>
-          <button onClick={logout}>Logout</button>
+          <button className={ isAuthenticated ? "" : "hidden"} onClick={logout}>Logout</button>
         </li>
       </ul>
       <h3>User is { isAuthenticated ? "Logged" : "Not logged"}</h3>
